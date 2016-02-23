a729e53681f45798da674100fa163c8e8ed8916b
LeXteRpro <lexter@hotmail.ca>
2016-02-22 20:58:06 -0500

    Added comments

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('home', { title: 'Orie Hulan' });
 });
 
-
+/* Get About Page */
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About Me' });
 });
 
+/* Get Projects Page */
 router.get('/projects', function(req, res, next) {
 	res.render('projects', { title: 'Projects'});
 });
 
+/* Get Services Page */
 router.get('/services', function(req, res, next) {
 	res.render('services', { title: 'Services'});
 });
 
+/* Get Contact Page */
 router.get('/contact', function(req, res, next) {
 	res.render('contact', { title: 'Contact Page'});
 });
